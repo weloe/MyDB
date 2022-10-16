98039640ee6905fd8ecd937be25589cb5588d3f9
weloe <1345895607@qq.com>
2022-10-16 19:09:02 +0800

    feat: 自定义异常

---
+++ src/main/java/com/weloe/mydb/exception/Error.java
@@ -0,0 +1,46 @@
+package com.weloe.mydb.exception;
+
+public class Error {
+    public static Exception BadTranFileException = new BadTranFileException();
+    public static FileExistsException FileExistsException = new FileExistsException();
+    public static FileCannotRWException FileCannotRWException = new FileCannotRWException();
+    public static FileNotExistsException FileNotExistsException = new FileNotExistsException();
+
+
+}
+class BadTranFileException extends RuntimeException{
+    public BadTranFileException() {
+        super();
+    }
+
+    public BadTranFileException(String message) {
+        super(message);
+    }
+}
+class FileExistsException extends RuntimeException{
+    public FileExistsException() {
+        super();
+    }
+
+    public FileExistsException(String message) {
+        super(message);
+    }
+}
+class FileCannotRWException extends RuntimeException{
+    public FileCannotRWException() {
+        super();
+    }
+
+    public FileCannotRWException(String message) {
+        super(message);
+    }
+}
+class FileNotExistsException extends RuntimeException{
+    public FileNotExistsException() {
+        super();
+    }
+
+    public FileNotExistsException(String message) {
+        super(message);
+    }
+}
