d581a64e9380a35fea87899d3e1a9a0b6eef7ea4
weloe <1345895607@qq.com>
2022-10-18 15:44:35 +0800

    feat: 修改异常处理

--- src/main/java/com/weloe/mydb/util/ExceptionHandler.java
+++ src/main/java/com/weloe/mydb/util/ExceptionHandler.java
@@ -5,4 +5,8 @@ public class ExceptionHandler {
     public static void handle(Exception e) throws Exception {
         throw e;
     }
+    public static void systemStop(Exception e){
+        e.printStackTrace();
+        System.exit(1);
+    }
 }
